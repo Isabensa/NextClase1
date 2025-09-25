1a900cbb09f6b06639d8efa27f21c4ace1f3c82b
Isabensa <bensadoncelia@gmail.com>
2025-09-24 22:26:03 -0300

    cambio de fuente

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+﻿// app/ui/fonts.ts
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'], // ajustá según lo que uses
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
